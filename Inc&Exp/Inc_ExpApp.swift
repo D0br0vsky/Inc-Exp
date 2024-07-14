19738379930b9f2e4620a517ff245b3b512e57d7
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-14 19:22:49 +0300

    Initial Commit

---
+++ Inc&Exp/Inc_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inc_ExpApp.swift
+//  Inc&Exp
+//
+//  Created by Dobrovsky on 14.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Inc_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
