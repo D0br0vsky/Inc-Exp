881910f7c798c84cb817a996c18ec2c8210a7848
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-15 15:23:43 +0300

    All clean and make another app

---
+++ Inc&Exp/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  Inc&Exp
+//
+//  Created by Dobrovsky on 15.07.2024.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
